42fe145b316e05d3700acee10d84ac7709845458
khannamudit84-droid <khanna.mudit84@googlemail.com>
2026-03-29 18:02:51 +0530

    Update test_model.py

--- scripts/test_model.py
+++ scripts/test_model.py
@@ -4,15 +4,11 @@ import sklearn
 
 print("🔍 sklearn version:", sklearn.__version__)
 
-print("🧪 Loading model...")
 model = joblib.load("models/best_model.pkl")
 
-print("📥 Loading data...")
 df = pd.read_csv("data/train.csv").head(5)
-
 X = df.drop("ProdTaken", axis=1, errors="ignore")
 
-print("🚀 Running predictions...")
 preds = model.predict(X)
 
 print("✅ Predictions:", preds)
