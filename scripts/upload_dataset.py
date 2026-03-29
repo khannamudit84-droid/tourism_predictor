7fdc02c53c86b0a3e0453ca76ca41946c33e804f
khannamudit84-droid <khanna.mudit84@googlemail.com>
2026-03-29 16:42:11 +0530

    Update upload_dataset.py

--- scripts/upload_dataset.py
+++ scripts/upload_dataset.py
@@ -1,1 +1,13 @@
+from huggingface_hub import login, upload_file
+import os
 
+login(token=os.environ["HF_TOKEN"])
+
+upload_file(
+    path_or_fileobj="tourism_project2/data/tourism.csv",
+    path_in_repo="data/raw/tourism.csv",
+    repo_id="Mudit1984/tourism_project2",
+    repo_type="dataset"
+)
+
+print("✅ Dataset uploaded to Hugging Face")
