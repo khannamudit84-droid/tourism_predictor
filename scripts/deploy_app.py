3db4779be6ed579f637af92ed371660924f8c0ea
khannamudit84-droid <khanna.mudit84@googlemail.com>
2026-03-29 16:44:03 +0530

    Update deploy_app.py

--- scripts/deploy_app.py
+++ scripts/deploy_app.py
@@ -1,1 +1,11 @@
+from huggingface_hub import login, upload_file
+import os
 
+login(token=os.environ["HF_TOKEN"])
+
+SPACE_REPO = "Mudit1984/tourism-predictor1"
+
+upload_file("app.py", "app.py", SPACE_REPO, repo_type="space")
+upload_file("requirements.txt", "requirements.txt", SPACE_REPO, repo_type="space")
+
+print("✅ App deployed to Hugging Face Space")
